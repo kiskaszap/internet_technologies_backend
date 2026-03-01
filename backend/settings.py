e9d19a960bfc6072fe848f96eb0cd34a0b6a82e8
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2026-03-01 18:16:20 +0000

    cloudinary implementation

--- backend/settings.py
+++ backend/settings.py
@@ -200,4 +200,13 @@ CLOUDINARY_STORAGE = {
     "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
 }
 
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+MEDIA_URL = "/media/"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
